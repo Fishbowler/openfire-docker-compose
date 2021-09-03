d8aff6a23faebff22eeb2f53d09ac7148042a2e4
Dan Caseley <dan@caseley.me.uk>
2021-09-03 18:46:39 +0100

    Apply Pumba for enforcing cluster disruptions. And, since there is no need for it anymore, remove the "alternative" network again.

--- scripts/get_logs.sh
+++ scripts/get_logs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 docker exec -t openfire-docker-compose_xmpp1_1 cat /usr/local/openfire/logs/all.log > 1all.log
 docker exec -t openfire-docker-compose_xmpp2_1 cat /usr/local/openfire/logs/all.log > 2all.log
-docker exec -t openfire-docker-compose_xmpp2_1 cat /usr/local/openfire/logs/jive.audit-20210202-000.log > jiveaudit.log
-
-#docker cp openfire-docker-compose_xmpp1_1:/usr/local/openfire/logs/all.log 1all.log
-#docker cp openfire-docker-compose_xmpp2_1:/usr/local/openfire/logs/all.log 2all.log
+docker exec -t openfire-docker-compose_xmpp3_1 cat /usr/local/openfire/logs/all.log > 3all.log
